b8d56d2431918559e3a68104ac50e305bdd5cf97
Kibe <roygithara6@gmail.com>
2023-02-16 18:50:08 +0300

    using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-entry point
+ * when return is zero: success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"/n);
+	return 0;
+}
