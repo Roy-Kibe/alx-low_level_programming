83c9643f9748d3bd06c6af3d7a50de3dfd92161b
Kibe <roygithara6@gmail.com>
2023-02-16 20:24:51 +0300

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main-entry point
- * when return is zero: success
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
