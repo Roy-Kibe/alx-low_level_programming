6d15f7bcb6db89a99cfc9d1eb6cfbcf7f4bda914
Kibe <roygithara6@gmail.com>
2023-02-16 21:11:42 +0300

    task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+
 /**
- * main-entry point
- * if return= zero correct
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
-		return 0;
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
 	
