7481ce88f2070ed5584cc468d2071daabac97677
Kibe <roygithara6@gmail.com>
2023-02-19 16:51:37 +0300

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,13 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		
-			putchar(c);
+		putchar(c);
 	}
 	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
