eb8b87475dcec9d3953c2fcf13b91f21a445e78a
Kibe <roygithara6@gmail.com>
2023-02-22 09:09:51 +0300

    task one

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-/*
- *main - entry
- *return 0 is success
+/**
+ * main - Prints _putchar as a message
+ *
+ * Return: Always 0 Success
  */
-
 int main(void)
 {
-
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
