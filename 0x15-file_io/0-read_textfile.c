5c45aa729e2618860dfed468c2677a93609f607a
Roy-Kibe <roygithara6@gmail.com>
2023-05-13 15:30:48 +0300

    task one

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,36 @@
-#include <stdio.h>
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-	{
+	int fd;
+	ssize_t rd, wr;
+	char *buf;
+
+	if (!filename)
 		return (0);
-	}
 
-	FILE *fp = fopen(filename, "r");
-	
-	if (fp == NULL)
-	{
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
-	}
 
-	char buffer[1024];
-	size_t read = 0;
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-	while (read < letters && !feof(fp))
-	{
-		size_t rem = letters - read;
-		size_t read_size = rem < sizeof(buffer) ? rem : sizeof(buffer);
-		size_t num_read = fread(buffer, sizeof(char), read_size, fp);
-		if (num_read < read_size)
-		{
-			break;
-		}
+	rd = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
 
-		size_t written;
-		written = fwrite(buffer, sizeof(char), num_read, stdout);
-		if (written != num_read)
-		{
-			fclose(fp);
-			return (ssize_t)0;
-		}
-		read += num_read;
-	}
+	close(fd);
 
-	fclose(fp);
-	return (ssize_t)read;
+	free(buf);
+
+	return (wr);
 }
-
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,6 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int create_file(const char *filename, char *text_content);
